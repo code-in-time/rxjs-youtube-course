55145307621f041d21cd62f3f5ff9fda3056fb87
code-in-time <andrew.crawford.za@gmail.com>
2018-05-01 18:23:30 +0200

    Update 1UnderstandingRxJS.js

--- 1UnderstandingRxJS.js
+++ 1UnderstandingRxJS.js
@@ -1,1 +1,23 @@
+<!-- https://www.youtube.com/watch?v=T9wOu11uU6U&list=PL55RiY5tL51pHpagYcrN9ubNLVXF8rGVi -->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+  
+  <button>click me</button>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/rxjs/5.0.0-rc.1/Rx.min.js"></script>
+  <script>
+    var button = document.querySelector('button');
 
+    // button.addEventListener('click', (e) => console.log(e));
+
+    Rx.Observable.fromEvent(button, 'click')
+      .throttleTime(5000)
+      .subscribe(e => console.log('click'));
+  </script>
+</body>
+</html>
